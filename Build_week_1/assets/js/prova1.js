3f42449898438e611a894e633301aa3c2bce69f7
Daniele Migani <danymigani@gmail.com>
2023-02-22 00:35:57 +0100

    modifica serale

---
+++ Build_week_1/assets/js/prova1.js
@@ -0,0 +1,32 @@
+const quizData = [    {        question: "Qual è il capitale dell'Italia?",        answers: [            "Milano",            "Roma",            "Firenze",            "Venezia"        ],
+        correctAnswer: 1
+    },
+    {
+        question: "Quale è il fiume più lungo del mondo?",
+        answers: [
+            "Nilo",
+            "Mississippi",
+            "Volga",
+            "Amazzoni"
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "Chi ha scritto la Divina Commedia?",
+        answers: [
+            "Giacomo Leopardi",
+            "Dante Alighieri",
+            "Petrarca",
+            "Alessandro Manzoni"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "In che anno è stata fondata Apple?",
+        answers: [
+            "1976",
+            "1980",
+            "1990"]
+    },
+]
+

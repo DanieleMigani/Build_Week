68576ffec99b0a9ff7b60b55fbc779862812f69b
Daniele Migani <danymigani@gmail.com>
2023-02-24 18:41:48 +0100

    modifica finali

--- Build_week_1/assets/js/Benchmark.js
+++ Build_week_1/assets/js/Benchmark.js
@@ -1,184 +1,263 @@
-// Array delle domande del quiz
-const questions = [
-  {
-    question: "What does CPU stand for??",
-    answers: [
-      { text: "Central Processing Unit", correct: true },
-      { text: "Central Process Unit", correct: false },
-      { text: "Computer Personal Unit", correct: false },
-      { text: "Central Processor Unit", correct: false },
-    ]
-  },
-  {
-    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
-    answers: [
-      { text: "Static", correct: false },
-      { text: "Final", correct: true },
-      { text: "Private", correct: false },
-      { text: "Public", correct: false },
-    ]
-  },
-  {
-      question: "The logo for Snapchat is a Bell.",
-      answers: [
-        { text: "True", correct: false },
-        { text: "False", correct: true },
-        { text: "", correct: false ,},
-        { text: "", correct: false },
-      ]
-    }, {
-      question: "Pointers were not used in the original C programming language; they were added later on in C++.",
-      answers: [
-        { text: "False", correct: true },
-        { text: "True", correct: false },
-        { text: "", correct: false },
-        { text: "", correct: false },
-      ]
-    }, {
-      question: "What is the most preferred image format used for logos in the Wikimedia database?",
-      answers: [
-        { text: ".gif", correct: false },
-        { text: ".jpeg", correct: false },
-        { text: ".svg", correct: true },
-        { text: ".png", correct: false },
-      ]
-    }, {
-      question: "In web design, what does CSS stand for?",
-      answers: [
-        { text: "Counter Strike: Source", correct: false },
-        { text: "Cascading Style Sheet", correct: true },
-        { text: "Corrective Style Sheet", correct: false },
-        { text: "Computer Style Sheet", correct: false },
-      ]
-    }, {
-      question: "What is the code name for the mobile operating system Android 7.0?",
-      answers: [
-        { text: "Nougat", correct: true },
-        { text: "Ice Cream Sandwich", correct: false },
-        { text: "Jelly Bean", correct: false },
-        { text: "Marshmallow", correct: false },
-      ]
-    }, {
-      question: "On Twitter, what is the character limit for a Tweet?",
-      answers: [
-        { text: "100", correct: false },
-        { text: "160", correct: false },
-        { text: "120", correct: false },
-        { text: "140", correct: true},
-      ]
-    }, {
-      question: "Linux was first created as an alternative to Windows XP.",
-      answers: [
-        { text: "False", correct: true },
-        { text: "True", correct: false },
-        { text: "", correct: false },
-        { text: "", correct: false },
-        
-      ]
-    }, {
-      question: "Which programming language shares its name with an island in Indonesia?",
-      answers: [
-        { text: "Python", correct: false },
-        { text: "Java", correct: true },
-        { text: "C", correct: false },
-        { text: "Jakarta", correct: false },
-      ]
+//timer
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+    info: {
+        color: "green",
+    },
+};
+
+const TIME_LIMIT = 30;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      > </path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+ 
+</div>
+`;
+startTimer();
+
+function resetTimer() {
+    timeLeft = TIME_LIMIT;
+    timePassed = 0;
+    clearInterval(timerInterval);
+    setCircleDasharray();
+    startTimer();
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        timePassed = timePassed += 1;
+        timeLeft = TIME_LIMIT - timePassed;
+        document.getElementById("base-timer-label").innerHTML =
+            formatTime(timeLeft);
+        setCircleDasharray();
+        setRemainingPathColor(timeLeft);
+
+        if (timeLeft === 0) {
+            resetTimer();
+        }
+    }, 1000);
+}
+
+function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+
+    return `${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(warning.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(info.color);
+        document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(warning.color);
+    }
+}
+
+function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+    const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+    document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+window.onload = function() {};
+
+//quiz
+const questions = [{
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What does CPU stand for?",
+        correct_answer: "Central Processing Unit",
+        incorrect_answers: [
+            "Central Process Unit",
+            "Computer Personal Unit",
+            "Central Processor Unit",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+        correct_answer: "Final",
+        incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "The logo for Snapchat is a Bell.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What is the most preferred image format used for logos in the Wikimedia database?",
+        correct_answer: ".svg",
+        incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "In web design, what does CSS stand for?",
+        correct_answer: "Cascading Style Sheet",
+        incorrect_answers: [
+            "Counter Strike: Source",
+            "Corrective Style Sheet",
+            "Computer Style Sheet",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "What is the code name for the mobile operating system Android 7.0?",
+        correct_answer: "Nougat",
+        incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "On Twitter, what is the character limit for a Tweet?",
+        correct_answer: "140",
+        incorrect_answers: ["120", "160", "100"],
+    },
+    {
+        category: "Science: Computers",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Linux was first created as an alternative to Windows XP.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "easy",
+        question: "Which programming language shares its name with an island in Indonesia?",
+        correct_answer: "Java",
+        incorrect_answers: ["Python", "C", "Jakarta"],
     },
 ];
 
-// Seleziona gli elementi HTML
-const questionNumber = document.getElementById("question-number");
-const questionText = document.querySelector("h1");
-const answerButtons = document.querySelectorAll(".answer");
-const score = document.getElementById("score");
-const scoreValue = document.getElementById("score-value");
-const timerValue = document.getElementById("timer-value");
-
-let currentQuestionIndex = 0;
-let correctAnswers = 0;
-let secondsLeft = 30;
-let timerInterval;
-
-// Funzione che inizia il quiz
-function startQuiz() {
-  showQuestion();
-  startTimer();
-}
-
-// Funzione che mostra la domanda corrente
-// Funzione che mostra la domanda corrente
+let currentQuestion = 0;
+let questionCounter = 1;
+let score = 0;
 
 function showQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
-  questionNumber.innerHTML = `QUESTION ${currentQuestionIndex + 1} <span>/${
-    questions.length
-  }</span>`;
-  questionText.innerText = currentQuestion.question;
-  answerButtons.forEach((button, index) => {
-    const answer = currentQuestion.answers[index];
-    if (answer.text === "") {
-      button.style.display = "none";
+    counter.innerText = `${questionCounter}`;
+    // recupera la domanda corrente
+    const question = questions[currentQuestion];
+
+    // mostra la domanda
+    document.querySelector("#question").innerHTML = question.question;
+
+    // mostra le risposte
+    let answersHTML = "";
+    for (const answer of question.incorrect_answers) {
+        answersHTML += `<button class="choice">${answer}</button>`;
+    }
+    answersHTML += `<button class="choice">${question.correct_answer}</button>`;
+    document.querySelector("#answers").innerHTML = answersHTML;
+
+    // imposta il timer di 30 secondi
+    timer = setTimeout(() => {
+        // passa alla domanda successiva
+        currentQuestion++;
+
+        if (currentQuestion < questions.length) {
+            showQuestion();
+            questionCounter++;
+            counter.innerText = `${questionCounter}`;
+        } else {
+            return window.location.assign(`Results.html?result=${score}`);
+        }
+    }, 30000);
+    resetTimer();
+}
+
+document.querySelector("#answers").addEventListener("click", (event) => {
+    // verifica se l'utente ha selezionato la risposta corretta
+    if (event.target.innerHTML === questions[currentQuestion].correct_answer) {
+        score += 1;
+    }
+
+    // passa alla domanda successiva
+    currentQuestion++;
+
+    // cancella il timer quando l'utente seleziona una risposta
+    clearTimeout(timer);
+
+    if (currentQuestion < questions.length) {
+        showQuestion();
+        questionCounter++;
+        counter.innerText = `${questionCounter}`;
     } else {
-      button.style.display = "inline-block";
-      button.innerText = answer.text;
-      button.onclick = () => {
-        checkAnswer(answer.correct);
-        clearInterval(timerInterval);
-        setTimeout(showNextQuestion, 1000);
-      };
+        return window.location.assign(`Results.html?result=${score}`);
     }
-  });
-}
+    return score;
+});
 
-// Funzione che controlla se la risposta data dall'utente è corretta
-function checkAnswer(isCorrect) {
-  if (isCorrect) {
-    correctAnswers++;
-  }
-}
-
-// Funzione che avvia il timer per ogni domanda
-function startTimer() {
-  secondsLeft = 30;
-  timerValue.innerText = secondsLeft;
-  timerInterval = setInterval(() => {
-    secondsLeft--;
-    timerValue.innerText = secondsLeft;
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-      showNextQuestion();
-    }
-  }, 1000);
-}
-
-// Funzione che mostra la domanda successiva
-function showNextQuestion() {
-  currentQuestionIndex++;
-  if (currentQuestionIndex === questions.length) {
-    showScore();
-  } else {
-    showQuestion();
-    startTimer();
-  }
-}
-
-let timerId; // variabile globale per salvare l'ID del timer
-
-// Funzione che mostra il risultato del quiz
-function showScore() {
-  clearInterval(timerId); // interrompi il timer se è ancora attivo
-  const timerDiv = document.getElementById("timer"); // ottieni il riferimento al div del timer
-  timerDiv.style.display = "none"; // nascondi il div del timer
-  questionNumber.innerText = "";
-  questionText.innerText = `Hai ottenuto ${correctAnswers} risposte corrette su ${questions.length}.`;
-  answerButtons.forEach(button => {
-    button.style.display = "none";
-  });
-  score.style.display = "block";
-  scoreValue.innerText = correctAnswers;
-}
-
-
-
-// Avvia il quiz quando la pagina è completamente caricata
-window.onload = startQuiz;
+// mostra la prima domanda
+showQuestion();
--- Build_week_1/assets/js/feedback.js
+++ Build_week_1/assets/js/feedback.js
@@ -1,63 +1,32 @@
-const stars = document.querySelectorAll('.stars img');
-const comment = document.querySelector('.commento');
+const stars = document.querySelectorAll(".star");
 
-let rating;
-
-stars.forEach(star => {
-    star.addEventListener('click', () => {
-        rating = star.getAttribute('data-value');
-
-        stars.forEach(otherStar => {
-            if (otherStar.getAttribute('data-value') <= rating) {
-                otherStar.classList.add('active');
-            } else {
-                otherStar.classList.remove('active');
-            }
-        });
+stars.forEach((star) => {
+  star.addEventListener("mouseover", () => {
+    const currentValue = parseInt(star.getAttribute("data-value"));
+    stars.forEach((s) => {
+      if (parseInt(s.getAttribute("data-value")) <= currentValue) {
+        s.classList.add("selected");
+      } else {
+        s.classList.remove("selected");
+      }
     });
+  });
 
-    star.addEventListener('mouseover', () => {
-        const value = star.getAttribute('data-value');
-
-        stars.forEach(otherStar => {
-            if (otherStar.getAttribute('data-value') <= value) {
-                otherStar.classList.add('hover');
-            } else {
-                otherStar.classList.remove('hover');
-            }
-        });
+  star.addEventListener("click", () => {
+    const currentValue = parseInt(star.getAttribute("data-value"));
+    stars.forEach((s) => {
+      s.classList.remove("selected");
+      if (parseInt(s.getAttribute("data-value")) <= currentValue) {
+        s.classList.add("selected");
+      }
     });
-
-    star.addEventListener('mouseout', () => {
-        stars.forEach(otherStar => {
-            otherStar.classList.remove('hover');
-        });
-    });
+  });
 });
-/* cancella il log del feedback*/
 
-
-function saveFeedback() {
-    const activeStars = document.querySelectorAll('.stars img.active');
-    const rating = activeStars.length;
-    const feedback = {
-        rating: rating,
-        comment: comment.value
-    };
-    comment.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-
-        }
-    });
-    console.log(feedback);
-    comment.value = '';
-}
-
-
-
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    saveFeedback();
-});
+document
+  .getElementById("comment")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      alert("Thanks for your feedback");
+    }
+  });
